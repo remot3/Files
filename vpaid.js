2e5aca2756c2b1b33fdc5f27ce0dbf25d55ce047
remot3 <dark__angel@outlook.com>
2025-06-27 09:15:42 +0200

    Update vpaid.js

--- vpaid.js
+++ vpaid.js
@@ -1,85 +1,85 @@
-(function() {
-  function VPAIDAd() {
-    var slot = null;
-    var videoSlot = null;
-    var eventsCallbacks = {};
+/**
+ * Minimal VPAID 2.0 JS Creative
+ * Compatible with videojs-vast-vpaid
+ */
+window.getVPAIDAd = function() {
+    function VPAIDAd() {
+        var slot = null;
+        var videoSlot = null;
+        var eventsCallbacks = {};
 
-    this.handshakeVersion = function(version) {
-      return '2.0';
-    };
+        this.handshakeVersion = function(version) {
+            return '2.0';
+        };
 
-    this.initAd = function(width, height, viewMode, desiredBitrate, creativeData, environmentVars) {
-      slot = environmentVars.slot;
-      videoSlot = environmentVars.videoSlot;
+        this.initAd = function(width, height, viewMode, desiredBitrate, creativeData, environmentVars) {
+            slot = environmentVars.slot;
+            videoSlot = environmentVars.videoSlot;
 
-      // Draw a simple overlay so we can see something
-      var overlay = document.createElement('div');
-      overlay.style.position = 'absolute';
-      overlay.style.top = '0';
-      overlay.style.left = '0';
-      overlay.style.width = width + 'px';
-      overlay.style.height = height + 'px';
-      overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-      overlay.style.color = 'white';
-      overlay.style.fontSize = '20px';
-      overlay.style.display = 'flex';
-      overlay.style.alignItems = 'center';
-      overlay.style.justifyContent = 'center';
-      overlay.innerHTML = 'VPAID Ad Loaded';
-      slot.appendChild(overlay);
+            var overlay = document.createElement('div');
+            overlay.style.position = 'absolute';
+            overlay.style.top = '0';
+            overlay.style.left = '0';
+            overlay.style.width = width + 'px';
+            overlay.style.height = height + 'px';
+            overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+            overlay.style.color = 'white';
+            overlay.style.fontSize = '20px';
+            overlay.style.display = 'flex';
+            overlay.style.alignItems = 'center';
+            overlay.style.justifyContent = 'center';
+            overlay.innerHTML = 'VPAID Ad Loaded';
+            slot.appendChild(overlay);
 
-      // Notify player that ad has loaded
-      callEvent('AdLoaded');
-      callEvent('AdStarted');
-    };
+            callEvent('AdLoaded');
+            callEvent('AdStarted');
+        };
 
-    this.startAd = function() {
-      callEvent('AdImpression');
-      callEvent('AdVideoStart');
-    };
+        this.startAd = function() {
+            callEvent('AdImpression');
+            callEvent('AdVideoStart');
+        };
 
-    this.stopAd = function() {
-      callEvent('AdStopped');
-    };
+        this.stopAd = function() {
+            callEvent('AdStopped');
+        };
 
-    this.skipAd = function() {
-      callEvent('AdSkipped');
-    };
+        this.skipAd = function() {
+            callEvent('AdSkipped');
+        };
 
-    this.resizeAd = function(width, height, viewMode) {};
+        this.resizeAd = function(width, height, viewMode) {};
 
-    this.pauseAd = function() {
-      callEvent('AdPaused');
-    };
+        this.pauseAd = function() {
+            callEvent('AdPaused');
+        };
 
-    this.resumeAd = function() {
-      callEvent('AdPlaying');
-    };
+        this.resumeAd = function() {
+            callEvent('AdPlaying');
+        };
 
-    this.expandAd = function() {
-      callEvent('AdExpandedChange');
-    };
+        this.expandAd = function() {
+            callEvent('AdExpandedChange');
+        };
 
-    this.collapseAd = function() {
-      callEvent('AdExpandedChange');
-    };
+        this.collapseAd = function() {
+            callEvent('AdExpandedChange');
+        };
 
-    this.subscribe = function(eventCallback, eventName) {
-      eventsCallbacks[eventName] = eventCallback;
-    };
+        this.subscribe = function(eventCallback, eventName) {
+            eventsCallbacks[eventName] = eventCallback;
+        };
 
-    this.unsubscribe = function(eventName) {
-      delete eventsCallbacks[eventName];
-    };
+        this.unsubscribe = function(eventName) {
+            delete eventsCallbacks[eventName];
+        };
 
-    function callEvent(eventName) {
-      if (eventsCallbacks[eventName]) {
-        eventsCallbacks[eventName]();
-      }
+        function callEvent(eventName) {
+            if (eventsCallbacks[eventName]) {
+                eventsCallbacks[eventName]();
+            }
+        }
     }
-  }
 
-  window.getVPAIDAd = function() {
     return new VPAIDAd();
-  };
-})();
+};
